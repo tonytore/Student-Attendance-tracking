f28bd7f1c4f7d04925d5f1496e1d04e89bd1c2f4
tonytor <teme4842@gmail.com>
2024-05-15 16:11:06 +0300

    install shadcn and setup the config file

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col justify-center items-center gap-2">
       <h1>student attendance tracking</h1>
+      <Button className="bg-blue-500">Subscribe</Button>
     </main>
   );
 }
