d180cb3d9a731c69e39e9d061bfc370804aa3c20
tonytor <teme4842@gmail.com>
2024-05-16 10:26:23 +0300

    merge card from card branch and deleted the branch

--- app/page.js
+++ app/page.js
@@ -6,7 +6,12 @@ export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center gap-2">
       <h1>student attendance tracking</h1>
-      <Card/>
+      <div className="flex gap-2">
+      <Card className="w-[400px] h-[400px] bg-slate-300"/>
+      <Card className="w-[400px] h-[400px] bg-red-600"/>
+      <Card className="w-[400px] h-[400px] bg-red-600"/>
+      <Card className="w-[400px] h-[400px] bg-red-600"/>
+      </div>
       <Button className="bg-blue-500">Subscribe</Button>
 
     </main>
