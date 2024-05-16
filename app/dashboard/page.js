bb33b320a9336d216bc34fe35294ea2a0edae635
tonytor <teme4842@gmail.com>
2024-05-16 22:37:03 +0300

    add darkmode

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,13 @@
-import React from 'react'
+"use client"
+
+import { useTheme } from 'next-themes'
+import React, { useEffect } from 'react'
+
 function Dashboard(){
+    const { setTheme } = useTheme()
+    useEffect(()=>{
+      setTheme('light')
+    },[])
     return(
         <div>Page</div>
     )
