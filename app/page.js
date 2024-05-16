304b8b99851e1a7ca3eca4077e0b814f6afabedb
tonytor <teme4842@gmail.com>
2024-05-16 10:04:15 +0300

    add card to home

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center gap-2">
       <h1>student attendance tracking</h1>
+      <Card/>
       <Button className="bg-blue-500">Subscribe</Button>
+
     </main>
   );
 }
