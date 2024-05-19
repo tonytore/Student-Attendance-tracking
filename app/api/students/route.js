caf1a24c2e677a6b094d0046dc7b3ba0fafe8876
tonytor <teme4842@gmail.com>
2024-05-19 14:11:31 +0300

    add post route fot student table

--- app/api/students/route.js
+++ app/api/students/route.js
@@ -1,6 +1,34 @@
 import db from "@/lib/db"
 import {  NextResponse } from "next/server";
 
+export async function POST(request){
+    try {
+        const { name, address, contact,} = await request.json();
+
+    
+
+        // const newBanner = {title,imageUrl,link,isActive}
+        const newBanner = await db.students.create({
+            data:{
+                name,
+                address,
+                contact,
+                
+            },
+        })
+       
+        return NextResponse.json(newBanner)
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            {
+                message: "Failed to create banner",
+                error,
+            },{status:500}
+        )
+    }
+}
+
 export async function GET(request){
     try {
         const students = await db.students.findMany({
